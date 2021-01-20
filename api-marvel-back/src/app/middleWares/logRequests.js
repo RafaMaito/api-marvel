b58cc0a24f2a39e8e669bbdb9731c0d2a76f3bbc
Rafael Maito <rafaelmaito@hotmail.com>
2021-01-19 22:11:37 -0300

    create arrLog

--- api-marvel-back/src/app/middleWares/logRequests.js
+++ api-marvel-back/src/app/middleWares/logRequests.js
@@ -1,8 +1,9 @@
+const arrLog = [];
 export default (request, response, next) => {
   const { method, url } = request;
 
   const logLabel = `[${method.toUpperCase()} ${url}]`;
-
+  arrLog.push(logLabel);
   console.log(logLabel);
 
   return next();
